6f763fabe11bc6c347cc545dc78b18069f271d5b
Abyan Maula <104250456+yanmaaa@users.noreply.github.com>
2022-08-30 09:22:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,52 +11,46 @@ from ubidots import build_payload, post_request
 
 
 def setup():
-    #GPIO Mode (BOARD / BCM) ; ini disemua file ada , jadi dipindah ke main
     GPIO.setmode(GPIO.BCM)
     init_ultrasonic()
     init_gps()
-    print("init")
-
-
+  
+  
 def loop():
     kiri, depan, kanan = get_ultrasonic_distance()
-    print("data ultrasonic")
-    # latitude, longitude = get_gps()
-    latitude = 100
-    longitude = 120
-    print("data gps")
-
+    latitude, longitude = get_gps()
+    
     if depan > 150:  # jika ke depan
         print("ke depan")
         play_speaker(speaker=3)
         decision = "ke depan"
-
+      
     else:
         if kiri > kanan:
             print("ke kiri")
             play_speaker(speaker=1)
             decision = "ke kiri"
-
+          
         elif kanan > kiri:
             print("ke kanan")
             play_speaker(speaker=2)
             decision = "ke kanan"
-
+          
         elif kanan < 50 and kiri < 50:
             print("ke mundur")
             play_speaker(speaker=4)
             decision = "ke mundur"
-
+      
     #.......
     # more logic here
     #.......
-
+    
     payload = build_payload(depan, kanan, kiri, latitude, longitude, decision)
     print("[INFO] Attemping to send data")
     post_request(payload)
     print("[INFO] finished")
-
-
+  
+  
 setup()
 while True:
     loop()
