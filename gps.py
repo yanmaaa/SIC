127ca581a9545a1bbf9655a6ed9f9a816dc7fa1e
Abyan Maula <104250456+yanmaaa@users.noreply.github.com>
2022-09-06 12:39:32 +0700

    Create gps.py

--- gps.py
+++ gps.py
@@ -6,12 +6,17 @@ ser = None
 
 def init_gps():
   global ser
-  ser = serial.Serial('/dev/ttyUSB0', 9600)  # open serial port
-
+  # ser = serial.Serial('/dev/ttyUSB0', 9600)  # open serial port
+  ser = serial.Serial('/dev/ttyUSB0', 9600, timeout= 1)  # open serial port
 def get_gps():
   global ser
   dataRaw = ser.read(8)  #6 ===> 2 x 4 of short data types (4 Bytes)
-  latitude, longitude = unpack("ff", dataRaw)  # 2 float data
+  if len(dataRaw) >= 8:
+    latitude, longitude = unpack("ff", dataRaw)  # 2 float data
+    # print("data gps")
+  else:
+    latitude, longitude = -7.811427550260671, 112.02549373888884
+    # print("data dummy")
   print(f"lat: {latitude}")
-  print(f"data2: {longitude}")
+  print(f"long: {longitude}")
   return latitude, longitude
